c97681941a8310a48ed1da20ed62f725c5a153cf
imsur <imsurusov@gmail.com>
2024-09-21 02:32:45 +0300

    Refactoring

--- src/main/java/ru/liga/parcelloader/models/Truck.java
+++ src/main/java/ru/liga/parcelloader/models/Truck.java
@@ -1,19 +1,26 @@
-package ru.liga.parcelloader.truck;
+package ru.liga.parcelloader.models;
 
-import ru.liga.parcelloader.parcel.Parcel;
+import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Truck {
-    private int width = 6;
-    private int height = 6;
+    private static final int DEFAULT_WIDTH = 6;
+    private static final int DEFAULT_HEIGHT = 6;
+
+    @Getter
+    private final int width;
+    @Getter
+    private final int height;
+    @Getter
+    private transient int weight = 0;
+
     private final char[][] space;
 
     public Truck(int width, int height) {
         this.width = width;
         this.height = height;
-
         space = new char[height][];
 
         for (int i = 0; i < height; i++) {
@@ -22,15 +29,41 @@ public class Truck {
         }
     }
 
+    public Truck(char[][] space) {
+        this.space = space;
+        this.height = space.length;
+        this.width = space[0].length;
+
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                if (space[i][j]  != ' ')
+                    weight++;
+    }
+
+    public Truck() {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    public int getCapacity() {
+        return height * width;
+    }
+
+    public char[][] getSpace() {
+        char[][] tempSpace = new char[height][width];
+        for (int i = 0; i < height; i++)
+            System.arraycopy(space[i], 0, tempSpace[i], 0, width);
+        return tempSpace;
+    }
+
     public boolean parcelCanBePlaced(Parcel parcel, int x, int y) {
-        if (y + parcel.getHeight() > space.length)
+        if (height < y + parcel.getHeight())
             return false;
 
-        if (x + parcel.getWidth() > space[0].length)
+        if (width < x + parcel.getWidth())
             return false;
 
         for (int i = 0; i < parcel.getHeight(); i++) {
-            char[] line = parcel.getLine(parcel.getHeight() - i - 1);
+            char[] line = parcel.getLayer(parcel.getHeight() - i - 1);
             for (int j = 0; j < line.length; j++) {
                 if (space[height - y - i - 1][x + j] != ' ')
                     return false;
@@ -47,7 +80,7 @@ public class Truck {
         return supportCount >= Math.ceil(parcel.getWidth() / 2.0);
     }
 
-    public boolean tryToLoadParcel(Parcel parcel) {
+    public boolean loadParcel(Parcel parcel) {
         for (int y = 0; y < height - parcel.getHeight() + 1; y++) {
             for (int x = 0; x < width - parcel.getWidth() + 1; x++) {
                 if (space[height - y - 1][x] != ' ')
@@ -55,9 +88,10 @@ public class Truck {
 
                 if (parcelCanBePlaced(parcel, x, y)) {
                     for (int i = 0; i < parcel.getHeight(); i++) {
-                        char[] line = parcel.getLine(parcel.getHeight() - i - 1);
-                        System.arraycopy(line, 0, space[height - y - i - 1], x, line.length);
+                        char[] layer = parcel.getLayer(i);
+                        System.arraycopy(layer, 0, space[height - y - i - 1], x, layer.length);
                     }
+                    weight += parcel.getWeight();
                     return true;
                 }
             }
@@ -66,18 +100,9 @@ public class Truck {
         return false;
     }
 
-    public int getHeight() {
-        return space.length;
-    }
-
-    public int getWidth() {
-        return space[0].length;
-    }
-
     @Override
     public String toString() {
-        return "\n" +
-                Arrays.stream(space)
+        return Arrays.stream(space)
                         .map(String::valueOf)
                         .map(line -> "+" + line + "+")
                         .collect(Collectors.joining("\n")) +
