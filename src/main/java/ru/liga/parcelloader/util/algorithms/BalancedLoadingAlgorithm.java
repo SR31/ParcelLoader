3ce7e92d80b053253588b57521e48ab0a6305487
imsur <imsurusov@gmail.com>
2024-10-10 05:58:55 +0300

    New name to algorithm

--- src/main/java/ru/liga/parcelloader/util/algorithms/BalancedLoadingAlgorithm.java
+++ src/main/java/ru/liga/parcelloader/util/algorithms/BalancedLoadingAlgorithm.java
@@ -1,6 +1,7 @@
 package ru.liga.parcelloader.util.algorithms;
 
 import org.springframework.stereotype.Component;
+import ru.liga.parcelloader.type.exception.UnableToLoadParcel;
 import ru.liga.parcelloader.type.model.entity.parcel.Parcel;
 import ru.liga.parcelloader.type.model.Truck;
 
@@ -9,11 +10,11 @@ import java.util.Comparator;
 import java.util.List;
 
 @Component("balanced")
-public class SameParcelsWeightAlgorithm implements LoadingAlgorithm {
+public class BalancedLoadingAlgorithm implements LoadingAlgorithm {
     /**
      * {@inheritDoc}
      * <br>
-     * Равномерная по весу погрузка посылок по машинам
+     * Сбалансированная по весу погрузка посылок по машинам
      */
     @Override
     public List<Truck> load(List<Truck> trucks, List<Parcel> parcels) {
@@ -35,8 +36,11 @@ public class SameParcelsWeightAlgorithm implements LoadingAlgorithm {
                 }
             }
 
-            if (!isParcelLoaded)
-                throw new IllegalArgumentException("Невозможно погрузить посылки");
+            if (!isParcelLoaded) {
+                throw new UnableToLoadParcel(
+                        "Невозможно загрузить посылку с именем " + parcel.getName()
+                );
+            }
         }
 
         return trucks;
