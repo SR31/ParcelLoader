fe308e369899b127256a84924c7109ba9649573e
imsur <imsurusov@gmail.com>
2024-10-10 05:58:22 +0300

    Another exception to work with parcels

---
+++ src/main/java/ru/liga/parcelloader/type/exception/UnableToLoadParcel.java
@@ -0,0 +1,7 @@
+package ru.liga.parcelloader.type.exception;
+
+public class UnableToLoadParcel extends RuntimeException {
+    public UnableToLoadParcel(String message) {
+        super(message);
+    }
+}
